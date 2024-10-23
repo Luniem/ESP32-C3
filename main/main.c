e325d755af1a1e5dd9827c57f68bb950fd1c83f1
Nico KNÜNZ <Nico.KNUENZ@bachmann.info>
2024-10-23 14:37:24 +0200

    fix: added some led-state for chi2-state

--- main/main.c
+++ main/main.c
@@ -3,6 +3,22 @@
 #include "led_strip.h"
 #include "portmacro.h"
 #include "math.h"
+#include "driver/gpio.h"
+
+#define CYCLE_TIME 50
+#define LED_GPIO 8
+#define LEFT_BUTTON_GPIO 9
+#define RIGHT_BUTTON_GPIO 2
+#define BUTTON_DEBOUNCE 5
+
+
+static int current_button_debounce = 0;
+
+static led_strip_handle_t led_strip;
+
+static int OK[25] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0};
+static int BAD[25] = { 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1};
+static int* mode = OK;
 
 #define SQUARE(x) ((x) * (x))
 
@@ -28,9 +44,8 @@ bool equalDistChi2(const uint32_t n[], uint32_t m, uint32_t n0, uint32_t chi2)
     return (x2 <= chi2);
 }
 
-void app_main(void)
-{
-    uint32_t *n = calloc(6, sizeof(uint32_t));
+static bool testRandomness(uint32_t dice_value) {
+    uint32_t *n = calloc(dice_value, sizeof(uint32_t));
     if (n == NULL)
     {
         printf("No memory :( \n");
@@ -40,26 +55,78 @@ void app_main(void)
 
         for (int i = 0; i < 10000000; i++)
         {
-            n[(nextRand() & 0xF) % 6] += 1;
+            n[nextRand() % dice_value] += 1;
         }
+        for (int i = 0; i < dice_value; i += 1)
+        {
+            printf("%d : %" PRIu32 "\n", i + 1, n[i]);
+        }
+
+        bool ok = equalDistChi2(n, dice_value, 10000000 / dice_value, 9);
+        free(n);
+        n = NULL;
+        return ok;
     }
 
-    for (int i = 0; i < 6; i += 1)
-    {
-        printf("%d : %" PRIu32 "\n", i + 1, n[i]);
+    return false;
+}
+
+static void configure_LED() {
+        /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_GPIO,
+        .max_leds = 25, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false,
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
+
+    gpio_config_t gpioConfigIn = {
+        .pin_bit_mask = (1 << LEFT_BUTTON_GPIO) | (1 << RIGHT_BUTTON_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = true,
+        .pull_down_en = false,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&gpioConfigIn);
+}
+
+void app_main(void)
+{
+    configure_LED();
+
+    while (1) {
+        int isClicked = gpio_get_level(LEFT_BUTTON_GPIO) == 0;
+        if(isClicked) {
+            for(int i =0; i< 25; i++) {
+                led_strip_set_pixel(led_strip, i, 0, 10, 0);
+            }
+            led_strip_refresh(led_strip);
+
+            bool ok = testRandomness(6);
+            if(ok) {
+                mode = OK;
+            } else {
+                mode = BAD;
+            }
+
+            for (int i = 0; i < 25; i++) {
+                
+                if(mode[i] == 0) {
+                    led_strip_set_pixel(led_strip, i, 0, 0, 0);
+                } else {
+                    led_strip_set_pixel(led_strip, i, 0, 0, 50);
+                }
+            }
+            led_strip_refresh(led_strip);
+        }
+
+        vTaskDelay(CYCLE_TIME / portTICK_PERIOD_MS);
     }
 
-    bool ok = equalDistChi2(n, 6, 10000000 / 6, 9);
-
-    if (ok)
-    {
-        printf("Is ok\n");
-    }
-    else
-    {
-        printf("Is not ok\n");
-    }
-
-    free(n);
-    n = NULL;
+    
 }
